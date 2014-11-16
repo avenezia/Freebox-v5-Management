bd668dbc8e9db53e58a710cfeef31b90437c6701
avenezia <alex19866891@hotmail.it>
2014-11-16 18:21:16 +0100

    Adding WiFi module state change

--- main.py
+++ main.py
@@ -7,14 +7,35 @@ import sys
 kUsernameControlName = "login"
 kPasswordControlName = "pass"
 
-def parseInputsForPostRequest(iDom):
-    requiredInputList = []
-    for inputName in ["wifi_random", "wifi_disable_radio", "wifi_active",
-                      "wifi_ssid", "wifi_channel_auto", "wifi_channel", 
-                      "wifi_ssid_hide", "wifi_key_type", "wifi_key", "action", "tpl"]:
-        inputValue = getValueForElement("input", inputName, iDom)
-        requiredInputList.append(inputName + ":" + inputValue)
-    return requiredInputList
+# Python 2.x doesn't offer Enums. Creating enum type using type method
+# https://docs.python.org/2/library/functions.html#type
+def enum(**enums):
+    return type('Enum', (), enums)
+
+WifiModuleAction = enum(ON = 1, OFF = 2, SWITCH = 3)
+
+def changeWiFiModuleState(iDom, iAction):
+    wifiModuleStateElement = getElementByName("input", "wifi_disable_radio", iDom)
+    assert wifiModuleStateElement
+    if iAction == WifiModuleAction.ON:
+        if isWifiModuleActive(wifiModuleStateElement):
+            print "Nothing to do, WiFi module is already ON"
+        else:
+            turnOnWiFiModule(wifiModuleStateElement)
+    elif iAction == WifiModuleAction.OFF:
+        if isWifiModuleActive(wifiModuleStateElement):
+            turnOffWiFiModule(wifiModuleStateElement)
+        else:
+            print "Nothing to do, WiFi module is already OFF"
+    elif iAction == WifiModuleAction.SWITCH:
+        switchWiFiModule(wifiModuleStateElement)
+    else:
+        raise Exception("Unknown action for WiFi module")
+
+def getElementByName(iElementType, iElementName, iDom):
+    searchedElement = iDom.find(iElementType, {"name": iElementName})
+    assert searchedElement is not None
+    return searchedElement
 
 def getLoginData():
     kUsageString = "Usage: " + sys.argv[0] + " username"
@@ -26,20 +47,20 @@ def getLoginData():
     password = getpass.getpass()
     return username, password
 
-def getValueForElement(iElementType, iElementName, iDom):
-    searchedElement = iDom.find(iElementType, {"name": iElementName})
-    assert searchedElement is not None
-    return searchedElement["value"]
-
 def isSuccessfulLogin(iLoginResponseStr):
     # In case of failure in the login phase, a div with class loginalert is available
     responseDom = BeautifulSoup(iLoginResponseStr)
     loginAlertDiv = responseDom.find("div", attrs={"class": "loginalert"})
     return True if loginAlertDiv is None else False
 
-def isWifiModuleActive(iDom):
+def isWifiModuleActiveOld(iDom):
     # The wifi module is controlled by <input name="wifi_disable_radio" type="hidden" value="0">
-    return getValueForElement("input", "wifi_disable_radio", iDom) == "0"
+    return getElementByName("input", "wifi_disable_radio", iDom)["value"] == "0"
+
+def isWifiModuleActive(iWiFiStateElement):
+    assert iWiFiStateElement
+    # The wifi module is controlled by <input name="wifi_disable_radio" type="hidden" value="0">
+    return iWiFiStateElement["value"] == "0"
 
 def parseCommandLineArgs():
     parser = argparse.ArgumentParser()
@@ -50,6 +71,34 @@ def parseCommandLineArgs():
     parser.add_argument("-r", help="Reboot the WiFi module to apply the changes", action="store_true")
     parser.add_argument("username", type=str, help="The username to login on Free site")
     parser.parse_args()
+
+def parseInputsForPostRequest(iDom):
+    requiredInputList = []
+    for inputName in ["wifi_random", "wifi_disable_radio", "wifi_active",
+                      "wifi_ssid", "wifi_channel_auto", "wifi_channel", 
+                      "wifi_ssid_hide", "wifi_key_type", "wifi_key", "action", "tpl"]:
+        inputValue = getElementByName("input", inputName, iDom)["value"]
+        requiredInputList.append(inputName + ":" + inputValue)
+    return requiredInputList
+
+def switchWiFiModule(iWiFiStateElement):
+    assert iWiFiStateElement
+    aElementValue = iWiFiStateElement["value"]
+    assert aElementValue == "0" or aElementValue == "1"
+    logMessage = "Switching from "
+    logMessage += "ON to OFF" if isWifiModuleActive(iWiFiStateElement) else "OFF to ON"
+    print logMessage
+    iWiFiStateElement["value"] = "1" if aElementValue == "0" else "0"
+
+def turnOffWiFiModule(iWiFiStateElement):
+    assert iWiFiStateElement
+    print "Turning OFF WiFi module"
+    iWiFiStateElement["value"] = "1"
+
+def turnOnWiFiModule(iWiFiStateElement):
+    assert iWiFiStateElement
+    print "Turning ON WiFi module"
+    iWiFiStateElement["value"] = "0"
 
 def main():
     parseCommandLineArgs()
@@ -67,9 +116,7 @@ def main():
         wifiPageResponse = browser.follow_link(text_regex=r"Param\xe9trer mon r\xe9seau WiFi")
         wifiDom = BeautifulSoup(wifiPageResponse.read())
         print parseInputsForPostRequest(wifiDom)
-        print isWifiModuleActive(wifiDom)
-        
-
+        print isWifiModuleActiveOld(wifiDom)
 
 if __name__ == '__main__':
     main()
